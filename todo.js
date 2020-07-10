86b5ca30dce3143fd03e62bef0bbb01ab43f9a79
Junil <wnsdlf9630@naver.com>
2020-07-11 01:10:10 +0900

    20200711 toDos.js deleteToDo complete

--- todo.js
+++ todo.js
@@ -1,33 +1,59 @@
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
-const TODOS_LS = 'toDos';
+const TODOS_LS = "currentToDos";
+const TODOS_CN = "form";
+let toDos = [];
 
-function paintToDo(text) {
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== JSON.parse(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDos(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  delBtn.innerText = "❌";
-  span.innerText = text;
+  toDoList.appendChild(li);
   li.appendChild(delBtn);
   li.appendChild(span);
-  toDoList.appendChild(li);
-  // delBtn.style.border = "none";
-  // delBtn.style.background = "white";
-
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  const toDoObj = {
+    text: text,
+    id: toDos.length + 1,
+  }
+  li.id = toDoObj.id;
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
+  const writeToDo = toDoInput.value;
   event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
+  paintToDos(writeToDo);
   toDoInput.value = "";
 }
 
-function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
 
+function loadToDos() {
+  const currentToDos = localStorage.getItem(TODOS_LS);
+  if (currentToDos !== null) {
+    const parsedToDos = JSON.parse(currentToDos);
+    const forEach = parsedToDos.forEach(function (toDo) {
+      paintToDos(toDo.text);
+    });
   }
 }
 
