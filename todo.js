23cde29c7bdf9d3c9c1045e8e68494d4d76eba7a
JunIl-git <64887129+JunIl-git@users.noreply.github.com>
2020-07-12 13:45:06 +0900

    complete todo.js

--- todo.js
+++ todo.js
@@ -2,18 +2,18 @@ const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 const TODOS_LS = "currentToDos";
-const TODOS_CN = "form";
 let toDos = [];
 
 function deleteToDo(event) {
-  const btn = event.target;
+  const btn = event.target
   const li = btn.parentNode;
   toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function (toDo) {
+  const filterFn = toDos.filter(function (toDo) {
     return toDo.id !== JSON.parse(li.id);
   });
-  toDos = cleanToDos;
-  saveToDos();
+  console.log(filterFn);
+  toDos = filterFn;
+  saveToDos()
 }
 
 function saveToDos() {
@@ -22,17 +22,19 @@ function saveToDos() {
 
 function paintToDos(text) {
   const li = document.createElement("li");
-  const delBtn = document.createElement("button");
   const span = document.createElement("span");
+  const delBtn = document.createElement("button");
   toDoList.appendChild(li);
   li.appendChild(delBtn);
   li.appendChild(span);
   delBtn.innerText = "❌";
+  delBtn.style.border = "none";
+  delBtn.style.background = "none";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   const toDoObj = {
-    text: text,
     id: toDos.length + 1,
+    text: text,
   }
   li.id = toDoObj.id;
   toDos.push(toDoObj);
@@ -40,20 +42,19 @@ function paintToDos(text) {
 }
 
 function handleSubmit(event) {
-  const writeToDo = toDoInput.value;
   event.preventDefault();
-  paintToDos(writeToDo);
+  const writeToDo = toDoInput.value;
   toDoInput.value = "";
+  paintToDos(writeToDo);
 }
-
 
 function loadToDos() {
   const currentToDos = localStorage.getItem(TODOS_LS);
   if (currentToDos !== null) {
     const parsedToDos = JSON.parse(currentToDos);
-    const forEach = parsedToDos.forEach(function (toDo) {
+    parsedToDos.forEach(function (toDo) {
       paintToDos(toDo.text);
-    });
+    })
   }
 }
 
