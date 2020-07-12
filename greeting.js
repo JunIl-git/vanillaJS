d54bd4956ef1081e0cd58840c262ec2517936c92
JunIl-git <64887129+JunIl-git@users.noreply.github.com>
2020-07-12 13:43:55 +0900

    complete greeting.js

--- greeting.js
+++ greeting.js
@@ -1,42 +1,41 @@
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
-
 const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
+const USER_CN = "showing";
 
-function saveName(text) {
+function saveLS(text) {
   localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
+function handleSubmit() {
   const currentValue = input.value;
-  paintGreeting(currentValue);
-  saveName(currentValue);
-}
-
-function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit)
+  saveLS(currentValue);
+  loadGreeting(currentValue);
 }
 
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
+  form.classList.remove(USER_CN);
+  greeting.classList.add(USER_CN);
   greeting.innerText = `Hello ${text}`;
 }
 
-function loadName() {
+function writeName() {
+  form.classList.add(USER_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function loadGreeting() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    askForName();
+    writeName()
   } else {
     paintGreeting(currentUser);
   }
 }
+
 function init() {
-  loadName();
+  loadGreeting();
 }
 
 init();
